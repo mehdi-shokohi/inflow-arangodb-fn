a2651cf1bb7ec52cba3ae7c4ab1dc740ce1a869c
mehdi <mehdi.shokohifar@gmail.com>
2024-04-07 08:01:15 +0330

    init inflow foxx

---
+++ README.md
@@ -0,0 +1,7 @@
+# hello-world
+
+A simple Hello World Foxx service
+
+## License
+
+The Apache-2.0 license. For more information, see the accompanying LICENSE file.
---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+const createRouter = require('@arangodb/foxx/router');
+const db = require('@arangodb').db;
+
+const router = createRouter();
+module.context.use(router);
+
+router.get('', (req, res) => {
+  inflowSend(res, ["query", "upsert", "save_doc"])
+}).response(["application/json"]);
+
+router.post('/query/run', (req, res) => {
+  const body = JSON.parse(req.body)
+  const result = db._query(`${body._params.q}`).toArray()
+  return inflowSend(res, result)
+}).response(["application/json"]);
+
+router.get('/query/load', (req, res) => {
+  return inflowSend(res, { "q": "for n in items limit 12000,1000 return n" })
+}).response(["application/json"]);
+
+router.get('/upsert/load', (req, res) => {
+  inflowSend(res, { "collection": "usersDoc" })
+}).response(["application/json"]);
+
+router.post('/upsert/run', (req, res) => {
+  let body = JSON.parse(req.body)
+  if (!body._params.collection) {
+    body._data.error = 'bad request - collection name is required';
+    return inflowSend(res, body._data)
+  }
+  if (!db._collection(body._params.collection)) {
+    db._createDocumentCollection(body._params.collection);
+  }
+  const colCreate = db._collection(body._params.collection);
+  colCreate.save(body._data)
+  body._data.createMessage = colCreate
+
+  inflowSend(res, body._data)
+}).response(["application/json"]);
+
+router.get('/save_doc/load', (req, res) => {
+  inflowSend(res, { "intoCollection": "usersDoc" })
+
+
+})
+router.post('/save_doc/run', (req, res) => {
+  let body = JSON.parse(req.body)
+  if (!body._params.intoCollection) {
+    body._data.error = 'bad request - collection name is required';
+    return inflowSend(res, body._data)
+  }
+  if (!db._collection(body._params.intoCollection)) {
+    db._createDocumentCollection(body._params.intoCollection);
+  }
+  const colCreate = db._collection(body._params.intoCollection);
+  colCreate.save(body._data)
+  body._data.createMessage = colCreate
+
+  inflowSend(res, body._data)
+})
+
+
+
+
+function inflowSend(res, data) {
+  res.write({ _data: data })
+}
+
